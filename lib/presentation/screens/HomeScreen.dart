7c4399778d35c06dba2ec088276cd2d323d8de0d
josh_MacBook <josuearroliga89@gmail.com>
2025-07-13 19:02:10 -0600

    Refactor Homescreen to pass Weather object to _ShowWeather and update icon retrieval

--- lib/presentation/screens/HomeScreen.dart
+++ lib/presentation/screens/HomeScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_string_interpolations
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_weather/config/iconSelector.dart';
@@ -25,7 +27,7 @@ class Homescreen extends StatelessWidget {
                   ),
                 );
               case WeatherLoadSuccess():
-                return _ShowWeather(state.weather.id);
+                return Center(child: _ShowWeather(state.weather));
               case WeatherLoadFailure():
                 return Center(child: Text("The weather couldn't be fetched"));
               case WeatherOperationInProgress():
@@ -49,21 +51,30 @@ class Homescreen extends StatelessWidget {
 }
 
 class _ShowWeather extends StatelessWidget {
-  final int id;
-  const _ShowWeather(this.id);
+  final Weather weather;
+  const _ShowWeather(this.weather);
 
   @override
   Widget build(BuildContext context) {
-    final icon = iconSelector[id]!;
+    final icon = iconSelector[weather.id]!;
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
+
       children: [
-        Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            child: Image.asset(icon, fit: BoxFit.fill),
-          ),
+        Container(
+          height: 200,
+          width: 200,
+          child: Image.asset(icon, fit: BoxFit.fill),
+        ),
+        SizedBox(height: 20),
+        Text(
+          "${double.parse(weather.currentTemperature).round()}°C",
+          style: TextTheme.of(context).displayLarge,
+        ),
+        SizedBox(height: 10),
+        Text(
+          "${weather.conditions}".toUpperCase(),
+          style: TextTheme.of(context).titleLarge,
         ),
       ],
     );
